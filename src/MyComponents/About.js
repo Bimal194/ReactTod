009a591215749fe3a2ca4a6e036a1c874940283b
Bimal shukla <69100830+Bimal194@users.noreply.github.com>
2021-06-01 12:24:16 +0530

    Add files via upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component. 
+            <p>Lorem ipsum dolor sit amet consectetur
+                 adipisicing elit. In expedita atque similique at dolores, 
+                 quis id alias deserunt? Alias, vero recusandae perspiciatis 
+                 dolorum dolores praesentium dolore harum eum at eius?</p>
+        </div>
+    )
+}
